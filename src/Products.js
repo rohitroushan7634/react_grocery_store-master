2d6ec0d1ff4cbcc67facd278788066246223fbf8
Rohit Roushan Raj <92080768+rohitroushan7634@users.noreply.github.com>
2022-04-13 12:05:58 +0530

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,7 @@
+var Products =[{"name":"Tomato","Price":40,"Button":"Buy","image":require('./Images/Tomato.png'),"key":""},
+                {"name":"Milk","Price":90,"Button":"Buy","image":require('./Images/Milk.png'),"key":""}, 
+                {"name":"Curd","Price":50,"Button":"Buy","image":require('./Images/Curd.png'),"key":""}, 
+                {"name":"Grapes","Price":80,"Button":"Buy","image":require('./Images/Grapes.png'),"key":""}, 
+                {"name":"Salt","Price":20,"Button":"Buy","image":require('./Images/Salt.png'),"key":""}];
+    
+export default Products
